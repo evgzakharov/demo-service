b95ec110a831a51dc15e82f46938019a1f23e833
Evgeniy Zakharov <evgzakharov88@gmail.com>
2019-06-12 23:54:24 +0300

    init spring-webflux-start

--- src/main/kotlin/demo/Controller.kt
+++ src/main/kotlin/demo/Controller.kt
@@ -1,74 +1,81 @@
 package demo
 
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
+import reactor.util.function.Tuple2
 import java.math.BigDecimal
-import java.util.concurrent.CompletableFuture
 
 @RestController
 class DemoController(
-    private val demoConfig: DemoConfig,
-    restTemplateBuilder: RestTemplateBuilder
+    private val demoConfig: DemoConfig
 ) {
-    private val restTemplate = restTemplateBuilder.build()
-
     @PostMapping
-    fun processRequest(@RequestBody serviceRequest: ServiceRequest): Response {
-        val authInfoFuture = CompletableFuture.supplyAsync {  getAuthInfo(serviceRequest.authToken) }
-        val userInfoFuture = authInfoFuture.thenApplyAsync { findUser(it.userId) }
+    fun processRequest(@RequestBody serviceRequest: Mono<ServiceRequest>): Mono<Response> {
+//        val authInfo = getAuthInfo(serviceRequest.authToken)
+//
+//        val userInfo = findUser(authInfo.userId)
+//
+//        val cardFromInfo = findCardInfo(serviceRequest.cardFrom)
+//        val cardToInfo = findCardInfo(serviceRequest.cardTo)
+//
+//        sendMoney(cardFromInfo.cardId, cardToInfo.cardId, serviceRequest.amount)
+//
+//        val paymentInfo = getPaymentInfo(cardFromInfo.cardId)
+//
+//        return SuccessResponse(
+//            amount = paymentInfo.currentAmount,
+//            userName = userInfo.name,
+//            userSurname = userInfo.surname,
+//            userAge = userInfo.age
+//        )
 
-        val cardFromInfo = CompletableFuture.supplyAsync { findCardInfo(serviceRequest.cardFrom) }
-        val cardToInfo = CompletableFuture.supplyAsync { findCardInfo(serviceRequest.cardTo) }
-
-        val waitAll = CompletableFuture.allOf(cardFromInfo, cardToInfo)
-
-        val paymentInfoFuture = waitAll
-            .thenApplyAsync {
-                sendMoney(cardFromInfo.get().cardId, cardToInfo.get().cardId, serviceRequest.amount)
-            }
-            .thenApplyAsync {
-                getPaymentInfo(cardFromInfo.get().cardId)
-            }
-
-        val paymentInfo = paymentInfoFuture.get()
-        val userInfo = userInfoFuture.get()
-
-        return SuccessResponse(
-            amount = paymentInfo.currentAmount,
-            userName = userInfo.name,
-            userSurname = userInfo.surname,
-            userAge = userInfo.age
-        )
+        TODO()
     }
 
-    private fun getPaymentInfo(cardId: Long): PaymentTransactionInfo {
-        return restTemplate.getForEntity("${demoConfig.payment}/{cardId}", PaymentTransactionInfo::class.java, cardId)
-            .body ?: throw RuntimeException("couldn't find card info with cardId='$cardId'")
+    private fun getPaymentInfo(cardId: Long): Mono<PaymentTransactionInfo> {
+        return WebClient.create().get()
+            .uri("${demoConfig.payment}/$cardId")
+            .retrieve()
+            .bodyToMono(PaymentTransactionInfo::class.java)
     }
 
-    private fun sendMoney(cardIdFrom: Long, cardIdTo: Long, amount: BigDecimal) {
+    private fun sendMoney(cardIdFrom: Long, cardIdTo: Long, amount: BigDecimal): Mono<Unit> {
         val paymentRequest = PaymentRequest(cardIdFrom, cardIdTo, amount)
 
-        restTemplate.postForEntity(demoConfig.payment, paymentRequest, PaymentSuccessInfo::class.java)
-            .body ?: throw RuntimeException("error while send payment request")
+        return WebClient.create().post()
+            .uri(demoConfig.payment)
+            .body(Mono.just(paymentRequest), PaymentRequest::class.java)
+            .retrieve()
+            .bodyToMono(PaymentSuccessInfo::class.java)
+            .map { Unit }
     }
 
-    private fun findCardInfo(cardNumber: String): CardInfo {
-        return restTemplate.getForEntity("${demoConfig.card}/{cardNumber}", CardInfo::class.java, cardNumber)
-            .body ?: throw RuntimeException("couldn't find card with number='$cardNumber'")
+    private fun findCardInfo(cardNumber: String): Mono<CardInfo> {
+        return WebClient.create().get()
+            .uri("${demoConfig.card}/$cardNumber")
+            .retrieve()
+            .bodyToMono(CardInfo::class.java)
     }
 
-    private fun findUser(userId: Long): UserInfo {
-        return restTemplate.getForEntity("${demoConfig.user}/{userId}", UserInfo::class.java, userId)
-            .body ?: throw RuntimeException("couldn't find user by userId='$userId'")
+    private fun findUser(userId: Long): Mono<UserInfo> {
+        return WebClient.create().get()
+            .uri("${demoConfig.user}/$userId")
+            .retrieve()
+            .bodyToMono(UserInfo::class.java)
     }
 
-    private fun getAuthInfo(token: String): AuthInfo {
-        return restTemplate.getForEntity("${demoConfig.auth}/{token}", AuthInfo::class.java, token)
-            .body ?: throw RuntimeException("couldn't find user by token='$token'")
+    private fun getAuthInfo(token: String): Mono<AuthInfo> {
+        return WebClient.create().get()
+            .uri("${demoConfig.auth}/$token")
+            .retrieve()
+            .bodyToMono(AuthInfo::class.java)
     }
 
+    private operator fun <T1, T2> Tuple2<T1, T2>.component1(): T1 = t1
 
+    private operator fun <T1, T2> Tuple2<T1, T2>.component2(): T2 = t2
 }
