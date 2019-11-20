923159e3dc961ef9168f376567c36f4bac697b81
Evgeniy Zakharov <evgzakharov88@gmail.com>
2019-11-20 14:25:39 +0300

    update versions

--- src/main/kotlin/demo/Application.kt
+++ src/main/kotlin/demo/Application.kt
@@ -2,8 +2,10 @@ package demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class DemoApplication
 
 fun main(args: Array<String>) {
