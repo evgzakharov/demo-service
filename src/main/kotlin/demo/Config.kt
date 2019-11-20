72b4ae72de035f3bac9807e3ed7de7ab73e5574d
Evgeniy Zakharov <evgzakharov88@gmail.com>
2019-11-20 15:09:37 +0300

    minor

--- src/main/kotlin/demo/Config.kt
+++ src/main/kotlin/demo/Config.kt
@@ -2,9 +2,7 @@ package demo
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.validation.annotation.Validated
 
-@Validated
 @ConstructorBinding
 @ConfigurationProperties(prefix = "url")
 data class DemoConfig(
