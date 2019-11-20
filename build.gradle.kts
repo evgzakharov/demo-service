6a52bccab2f3e6b2a021de2fbbca3f4a7d289947
Evgeniy Zakharov <evgzakharov88@gmail.com>
2019-11-20 14:25:39 +0300

    update versions

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.3.60"
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+
+    id("org.springframework.boot") version "2.2.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.7.RELEASE" 
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.getByName("compileKotlin").dependsOn("processResources")
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-progressive")
+        jvmTarget = "12"
+    }
+}
--- src/main/kotlin/demo/Config.kt
+++ src/main/kotlin/demo/Config.kt
@@ -1,11 +1,13 @@
 package demo
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.validation.annotation.Validated
 import javax.validation.constraints.Pattern
 
-@ConfigurationProperties(prefix = "url")
 @Validated
+@ConstructorBinding
+@ConfigurationProperties(prefix = "url")
 data class DemoConfig(
     val auth: String,
     val card: String,
