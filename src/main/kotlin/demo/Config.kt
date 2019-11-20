a5514c4ba5efe8bb1db537bdb96f9a24fb05ce1f
Evgeniy Zakharov <evgzakharov88@gmail.com>
2019-11-20 14:32:43 +0300

    update versions

--- src/main/kotlin/demo/Config.kt
+++ src/main/kotlin/demo/Config.kt
@@ -1,10 +1,12 @@
 package demo
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.validation.annotation.Validated
 
-@ConfigurationProperties(prefix = "url")
 @Validated
+@ConstructorBinding
+@ConfigurationProperties(prefix = "url")
 data class DemoConfig(
     val auth: String,
     val card: String,
