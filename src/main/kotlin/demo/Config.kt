f1cc47005bb636b5ba5c685e3843bad417745793
Evgeniy Zakharov <evgzakharov88@gmail.com>
2019-11-20 15:09:04 +0300

    minor

--- src/main/kotlin/demo/Config.kt
+++ src/main/kotlin/demo/Config.kt
@@ -2,17 +2,12 @@ package demo
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.validation.annotation.Validated
-import javax.validation.constraints.Pattern
 
-@Validated
 @ConstructorBinding
 @ConfigurationProperties(prefix = "url")
 data class DemoConfig(
     val auth: String,
     val card: String,
     val payment: String,
-    val user: String,
-    @Pattern(regexp = "\\d+")
-    val testDigit: String
+    val user: String
 )
